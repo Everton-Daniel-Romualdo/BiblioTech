a36f55a7241016ac267a9879b29f911f50bde16b
Everton Daniel <evertonrdaniel@gmail.com>
2024-05-14 11:50:34 -0300

    Commit inicial do projeto

---
+++ src/programa/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Classe Principal De interação com o usuario
+ * @author Everton Daniel
+ */
+package programa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
